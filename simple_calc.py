83a27d72bf5582ba3c84c81a53423e16ef47eb56
Otemory <kalacheva.alena@gmail.com>
2021-03-04 22:51:01 +0300

    simple calc first commit

---
+++ simple_calc.py
@@ -0,0 +1,23 @@
+
+a, b, op = float(input("number 1: ")), float(input("number 2: ")), input("operation: ")
+
+if op in ["mod", "pow", "div", "+", "-", "/", "*"]:
+    if op in ["div", "/", "mod"] and b == 0:
+        print("Деление на 0!")
+    else:
+        if op == "mod":
+            print (a % b)
+        elif op == "pow":
+            print(a ** b)
+        elif op == "div":
+            print(a // b)
+        elif op == "+":
+            print(a + b)
+        elif op == "-":
+            print(a - b)
+        elif op == "*":
+            print(a * b)
+        elif op == "/":
+            print(a / b)
+else:
+    print("wrong operation")
