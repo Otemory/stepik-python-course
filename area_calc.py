4d568b5a091178bb161204e28dfe824344602712
Otemory <kalacheva.alena@gmail.com>
2021-03-05 00:28:14 +0300

    area_calc done2

--- area_calc.py
+++ area_calc.py
@@ -4,22 +4,23 @@ if figure_name == "треугольник":
     if a > 0 and b > 0 and c > 0:
         p = (a + b + c) / 2
         s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
-        print(float(s))
+        # print(float(s))
     else:
         print("Wrong")
 elif figure_name == "прямоугольник":
     a, b = float(input()), float(input())
     if a > 0 and b > 0:
         s = a * b
-        print(float(s))
+        # print(float(s))
     else:
         print("Wrong")
 elif figure_name == "круг":
     r = float(input())
     if r > 0:
         s = 3.14 * r ** 2
-        print(float(s))
+        # print(float(s))
     else:
         print("Wrong")
 else:
     print("Wrong figure")
+print(s)
