1169573e5f17ee4fe782841fdbed812c9e06f680
Otemory <kalacheva.alena@gmail.com>
2021-03-04 23:51:37 +0300

    area_calc triangle in progress

--- area_calc.py
+++ area_calc.py
@@ -3,6 +3,11 @@ if figure_name == "треугольник":
     a = int(input())
     b = int(input())
     c = int(input())
+    if a > 0 and b > 0 and c > 0:
+        p = (a + b + c) / 2
+        s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+    else:
+        print("Wrong")
 elif figure_name == "прямоугольник":
     a = int(input())
     b = int(input())
