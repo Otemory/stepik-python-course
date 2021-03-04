6e00e37edb14c6b3804e471b49d2749642b28923
Otemory <kalacheva.alena@gmail.com>
2021-03-04 23:57:53 +0300

    area_calc triangle in progress

--- area_calc.py
+++ area_calc.py
@@ -6,12 +6,23 @@ if figure_name == "треугольник":
     if a > 0 and b > 0 and c > 0:
         p = (a + b + c) / 2
         s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+        print(s)
     else:
         print("Wrong")
 elif figure_name == "прямоугольник":
     a = int(input())
     b = int(input())
+    if a > 0 and b > 0:
+        s = a * b
+        print(s)
+    else:
+        print("Wrong")
 elif figure_name == "круг":
     r = int(input())
+    if r > 0:
+        s = 3.14 * r ** 2
+        print(s)
+    else:
+        print("Wrong")
 else:
     print("Wrong figure")
