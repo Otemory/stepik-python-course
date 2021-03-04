b922d5e65d2ade3c6b9d10720ed14beb9853fa79
Otemory <kalacheva.alena@gmail.com>
2021-03-04 23:31:18 +0300

    area_calc work in progress

--- area_calc.py
+++ area_calc.py
@@ -3,3 +3,10 @@ if figure_name == "треугольник":
     a = int(input())
     b = int(input())
     c = int(input())
+elif figure_name == "прямоугольник":
+    a = int(input())
+    b = int(input())
+elif figure_name == "круг":
+    r = int(input())
+else:
+    print("Wrong figure")
