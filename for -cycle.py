29d57907f4044b9175fa647325e3c4214c3ce3ae
Otemory <kalacheva.alena@gmail.com>
2021-03-05 18:57:39 +0300

    for cycle ну такое

--- for -cycle.py
+++ for -cycle.py
@@ -23,14 +23,15 @@ for i in range(n):
 Следуйте формату вывода из примера, для разделения элементов внутри строки используйте '\t' — символ табуляции. 
 Заметьте, что левым столбцом и верхней строкой выводятся сами числа из заданных отрезков — заголовочные столбец и строка таблицы."""
 
-a, b, c, d = input().split()
-a = int(a)
-b = int(b)
-c = int(c)
-d = int(d)
+# a, b, c, d = int(input()), int(input()), int(input()), int(input())
+a, b, c, d = 7, 10, 5, 6
 
-if a < 10 and b < 10 and c < 10 and d < 10:
-    print("ok")
+if a <= 10 and b <= 10 and c <= 10 and d <= 10:
+    for i in range(a, b +1):
+        for j in range(c, d + 1):
+            print(i * j, end="\t")
+        print()
+
 
 else:
     print("error")
