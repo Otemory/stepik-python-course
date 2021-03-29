270721e0658a22272214213e990516bdc186eeac
Otemory <kalacheva.alena@gmail.com>
2021-03-29 23:38:55 +0300

    game of choise progress 2

--- game-of-choise.py
+++ game-of-choise.py
@@ -4,19 +4,27 @@ num = randint(1, 100)
 
 
 def is_valid(number):
-    if number in range(1, 100):
-        return True
+    if number.isdigit():
+        if int(number) in range(1, 101):
+            return True
     else:
         return False
 
 
 print('Добро пожаловать в числовую угадайку')
+
 while True:
-    guess = int(input('Enter your guess: '))
-    if guess > num:
-        print('Слишком много, попробуйте еще раз')
-    elif guess < num:
-        print('Слишком мало, попробуйте еще раз')
+    guess = input('Enter your guess: ')
+    print(is_valid(guess))
+    if not is_valid(guess):
+        print('А может быть все-таки введем целое число от 1 до 100?')
+        continue
     else:
-        print('Вы угадали, поздравляем!')
-        break
+        guess = int(guess)
+        if guess > num:
+            print('Слишком много, попробуйте еще раз')
+        elif guess < num:
+            print('Слишком мало, попробуйте еще раз')
+        else:
+            print('Вы угадали, поздравляем!')
+            break
