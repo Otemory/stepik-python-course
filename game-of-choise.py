f1bc6bb020df850fc2a0e43ef38a4782dec3df1e
Otemory <kalacheva.alena@gmail.com>
2021-03-29 22:38:12 +0300

    game of choise first commit

--- game-of-choise.py
+++ game-of-choise.py
@@ -1,1 +1,11 @@
 from random import *
+num = randint(1, 100)
+while True:
+    guess = int(input('Enter your guess: '))
+    if guess > num:
+        print('Слишком много, попробуйте еще раз')
+    elif guess < num:
+        print('Слишком мало, попробуйте еще раз')
+    else:
+        print('Вы угадали, поздравляем!')
+        break
