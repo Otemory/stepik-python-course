f090b33a35e2479fa8cb77bc5ec1083744cbff6d
Otemory <kalacheva.alena@gmail.com>
2021-03-29 23:05:31 +0300

    game of choise progress

--- game-of-choise.py
+++ game-of-choise.py
@@ -1,5 +1,16 @@
 from random import *
+
 num = randint(1, 100)
+
+
+def is_valid(number):
+    if number in range(1, 100):
+        return True
+    else:
+        return False
+
+
+print('Добро пожаловать в числовую угадайку')
 while True:
     guess = int(input('Enter your guess: '))
     if guess > num:
