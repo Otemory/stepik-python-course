ef1ccc5d26046939f9d32973cad0b5ec6e62f8e6
Otemory <kalacheva.alena@gmail.com>
2021-03-30 12:01:17 +0300

    game of choise added more games

--- game-of-choise.py
+++ game-of-choise.py
@@ -1,7 +1,5 @@
 from random import *
 
-num = randint(1, 100)
-
 
 def is_valid(number):
     if number.isdigit():
@@ -12,22 +10,28 @@ def is_valid(number):
 
 
 print('Добро пожаловать в числовую угадайку')
-cnt = 0
-while True:
-    guess = input('Ведите число от 1 до 100: ')
+answer = input('Хотите сыграть? да/нет ')
+while answer.lower() == 'да':
+    num = randint(1, 100)
 
-    if not is_valid(guess):
-        print('А может быть все-таки введем целое число от 1 до 100?')
-        continue
-    else:
-        guess = int(guess)
-        if guess > num:
-            print('Слишком много, попробуйте еще раз')
-            cnt += 1
-        elif guess < num:
-            print('Слишком мало, попробуйте еще раз')
-            cnt += 1
+    cnt = 0
+    while True:
+        guess = input('Ведите число от 1 до 100: ')
+
+        if not is_valid(guess):
+            print('А может быть все-таки введем целое число от 1 до 100?')
+            continue
         else:
-            print(f'Вы угадали c {cnt} попытки, поздравляем!')
-            break
-print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+            guess = int(guess)
+            if guess > num:
+                print('Слишком много, попробуйте еще раз')
+                cnt += 1
+            elif guess < num:
+                print('Слишком мало, попробуйте еще раз')
+                cnt += 1
+            else:
+                print(f'Вы угадали c {cnt} попытки, поздравляем!')
+                break
+    answer = input('Хотите сыграть еще? да/нет ')
+else:
+    print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
